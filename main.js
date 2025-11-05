4458a35f781dbe0c56d949abfa06af744705ca2a
Isabella Harrington <isabellaharrington@gmail.com>
2025-11-05 07:32:56 -0500

    Recommitted main.js changes as the introText code was not saved

--- main.js
+++ main.js
@@ -4,11 +4,16 @@ function loadIntroForm() {
     const introForm = document.createElement("form");
     introForm.classList.add("intro-form");
 
+    const introText = document.createElement("p");
+    introText.textContent = "Welcome to the Moogle Browser Assessment! This is a 20-question-long assessment meant to determine your level of basic browser knowledge. Each question is worth 5 points, but if you don't know the answer to a question, don't panic! There will be a help button available for each question, and if you're still unsure you can always skip ahead.";
+    introText.classList.add("intro-text");
+
     const startButton = document.createElement("button");
     startButton.type = "submit";
     startButton.textContent = "Start";
     startButton.classList.add("start-button");
 
+    introForm.appendChild("intro-text");
     introForm.appendChild(startButton);
 }
 
