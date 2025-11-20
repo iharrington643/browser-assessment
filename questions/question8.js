4eedf0dafb300f3c65ccccde449e38432a7a9483
Isabella Harrington <isabellaharrington@gmail.com>
2025-11-20 14:50:35 -0500

    Adjusted the themeButtons event listener to permanently change the opacity of the button associated with the selected theme until another theme is selected

--- questions/question8.js
+++ questions/question8.js
@@ -313,7 +313,10 @@ export function question8Select() {
 
     themeButtons.forEach(button => {
         button.addEventListener('click', function() {
+            themeButtons.forEach(btn => btn.style.opacity = '0.65');
+
             selectedTheme = this;
+            this.style.opacity = '1';
         })
     })
 
