3c17eee7869f1c99f99adfe1c33bfb5123d31304
Isabella Harrington <isabellaharrington@gmail.com>
2025-12-01 07:01:36 -0500

    Created results.js file with loadResults function based on the loadIntroForm function, imported it into the final question

---
+++ results.js
@@ -0,0 +1,24 @@
+import { userData } from './main.js';
+
+function loadIntroForm() {
+    const body = document.getElementById("assessment-body");
+    
+    const introForm = document.createElement("form");
+    introForm.classList.add("intro-form");
+
+    const introText = document.createElement("p");
+    introText.textContent = "Welcome to the Moogle Browser Assessment! This is a 20-question-long assessment meant to determine your level of basic browser knowledge. Each question is worth 5 points, but if you don't know the answer to a question, don't panic! There will be a help button available for each question, and if you're still unsure, you can always skip ahead!";
+    introText.classList.add("intro-text");
+
+    const startButton = document.createElement("button");
+    startButton.type = "button";
+    startButton.textContent = "START";
+    startButton.classList.add("start-button");
+
+    const mouseClickSound = new Audio('./sounds/mouse-click.mp3');
+
+    introForm.appendChild(introText);
+    introForm.appendChild(startButton);
+
+    body.appendChild(introForm);
+}
