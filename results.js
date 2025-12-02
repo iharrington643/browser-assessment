42a2b4433dde46781e26e3314ea8caa6b38a5c60
Isabella Harrington <isabellaharrington@gmail.com>
2025-12-02 08:38:56 -0500

    Added event listener to tryAgainButton for it to play a clicking sound and refresh the page on click

--- results.js
+++ results.js
@@ -6,17 +6,17 @@ export function loadResults() {
 
     const body = document.getElementById("assessment-body");
     
-    const introForm = document.createElement("form");
-    introForm.classList.add("intro-form");
+    const resultForm = document.createElement("form");
+    resultForm.classList.add("result-form");
 
     const introText = document.createElement("p");
-    introText.textContent = `Congratulations!  You completed the assessment with a score of ${result}.  This means that you answered ${resultOfTwenty} questions correctly!`;
+    introText.textContent = `Congratulations!  You completed the assessment with a score of ${result}%.  This means that you answered ${resultOfTwenty} questions correctly!`;
     introText.classList.add("intro-text");
 
-    const startButton = document.createElement("button");
-    startButton.type = "button";
-    startButton.textContent = "START";
-    startButton.classList.add("start-button");
+    const tryAgainButton = document.createElement("button");
+    tryAgainButton.type = "button";
+    tryAgainButton.textContent = "TRY AGAIN";
+    tryAgainButton.classList.add("try-again-button");
 
     const mouseClickSound = new Audio('./sounds/mouse-click.mp3');
 
@@ -24,4 +24,9 @@ export function loadResults() {
     introForm.appendChild(startButton);
 
     body.appendChild(introForm);
+
+    tryAgainButton.addEventListener('click', function() {
+        mouseClickSound.play();
+        window.location.reload();
+    })
 }
