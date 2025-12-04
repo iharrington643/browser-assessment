7267393d822df61f712c721e99ff135807dc7941
Isabella Harrington <isabellaharrington@gmail.com>
2025-12-03 19:45:54 -0500

    Edited assessment description in main.js

--- main.js
+++ main.js
@@ -11,7 +11,7 @@ function loadIntroForm() {
     introForm.classList.add("intro-form");
 
     const introText = document.createElement("p");
-    introText.textContent = "Welcome to the Moogle Browser Assessment! This is a 20-question-long assessment meant to determine your level of basic browser knowledge. Each question is worth 5 points, but if you don't know the answer to a question, don't panic! There will be a help button available for each question, and if you're still unsure, you can always skip ahead!";
+    introText.textContent = "Welcome to the Moogle Browser Assessment! This is a 20-question-long assessment meant to determine your level of basic browser knowledge. Each question is worth 5 points, but if you don't know the answer to a question, don't panic! There will be a hint button available for each question, and if you're still unsure, you can always skip ahead!";
     introText.classList.add("intro-text");
 
     const startButton = document.createElement("button");
